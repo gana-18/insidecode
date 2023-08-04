1effb2c9fe04947d1dcf723ebf8d3897b3b29905
Gana Sai Teja <velichetiganasaiteja@gmail.com>
2023-08-04 17:22:11 +0530

    useeffect moved to home

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,54 +1,3 @@
-/*const express=require('express')
-const router=express.Router()
-const passport=require('passport')
-
-
-router.get('/login/success',(req,res)=>{
-   if(req.user){
-      res.status(200).json({
-         success:true,
-         message:"User has successfully authenticated",
-         user:req.user
-      })
-   }
-})
-
-router.get('/login/failed',(req,res)=>{
-   res.status(400).json({
-      success:false,
-      message:"User failed to authenticate"
-   })
-})
-
-// @desc  AUTH WITH GOOGLE
-// @Method GET WITH GOOGLE
-router.get('/google',passport.authenticate('google',{scope:['profile']}))
-
-
-// @desc  GOOGLE AUTH CALLBACK
-// @Method GET GOOGLE CALLBACK
-/*router.get('/google/callback',passport.authenticate('google',{failureRedirect:'/'}),(req,res)=>{
-   res.json(req.user)
-});
-
-router.get('/google/callback',passport.authenticate("google",{
-   successRedirect:process.env.CLIENT_URL,
-   failureRedirect:"/login/failed"
-}))
-
-// @desc  LOGOUT USER
-// @Method GET LOGOUT
-router.get('/logout',(req,res)=>{
-   req.logout(function(err){
-         if(err){
-              return next(err);
-         }
-   })  
-    res.redirect(`${process.env.CLIENT_URL}`)
-})
-
-
-module.exports=router*/
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
@@ -78,7 +27,7 @@ router.get('/login/failed', (req, res) => {
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 
 router.get('/google/callback', passport.authenticate("google", {
-   successRedirect: `${process.env.CLIENT_URL}`,
+   successRedirect: `${process.env.CLIENT_URL}`+"/home",
   failureRedirect: '/login/failed'
 }), (req, res) => {
   // Send the authentication result as JSON
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,21 +3,32 @@ import { useState, useEffect } from 'react';
 import { Routes, Route, NavLink, Link, useLocation } from "react-router-dom";
 import {useSelector,useDispatch} from 'react-redux';
 import {fetchFollowingPosts} from '../features/post/postSlice';
+import {login} from '../features/auth/authSlice';
 
 function Home() {
-  const {user} = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const [user,setUser] = useState(null);
+  const auth = useSelector((state) => state.auth);
   useEffect(() => {
-    if(!user) {
-      window.location.href = "/";
+    if (!auth.user) {
+      dispatch(login());
     }
-  }, [user]);
+  }, [auth.user, dispatch]);
+
+  useEffect(() => {
+    if (auth.status === "succeeded") {
+      setUser(auth.user);
+    } else if (auth.status === "failed") {
+      setUser(null);
+    }
+  }, [auth.status, auth.user]);
+
   const {posts} = useSelector((state) => state.post);
   const [followingPosts, setFollowingPosts] = useState([]);
   const location = useLocation();
-  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchFollowingPosts(user._id)).then((response) => {
+    dispatch(fetchFollowingPosts(user?user._id:{})).then((response) => {
       setFollowingPosts(response.payload);
     });
   },[ dispatch])
