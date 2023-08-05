df1dd266e4c985c029de860a33d4599d88700367
Gana Sai Teja <velichetiganasaiteja@gmail.com>
2023-08-05 23:25:47 +0530

    auth changed

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -27,23 +27,9 @@ router.get('/login/failed', (req, res) => {
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 
 router.get('/google/callback', passport.authenticate("google", {
-   successRedirect: `${process.env.CLIENT_URL}`+"/home",
+   successRedirect: `${process.env.CLIENT_URL}`,
   failureRedirect: '/login/failed'
-}), (req, res) => {
-  // Send the authentication result as JSON
-  if (req.user) {
-    res.status(200).json({
-      success: true,
-      message: "User has successfully authenticated",
-      user: req.user
-    });
-  } else {
-    res.status(401).json({
-      success: false,
-      message: "User not authenticated"
-    });
-  }
-});
+}))
 
 router.get('/logout', (req, res) => {
   req.logout(function (err) {
